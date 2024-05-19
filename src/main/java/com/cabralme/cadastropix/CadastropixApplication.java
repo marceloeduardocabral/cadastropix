9483fcb788597b375c54b7e5f05f7a167b552de9
Marcelo Cabral <cabralme@gmail.com>
2024-05-19 14:42:50 -0300

    Projeto inicial

---
+++ src/main/java/com/cabralme/cadastropix/CadastropixApplication.java
@@ -0,0 +1,13 @@
+package com.cabralme.cadastropix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastropixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastropixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cabralme/cadastropix/CadastropixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cabralme.cadastropix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastropixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
