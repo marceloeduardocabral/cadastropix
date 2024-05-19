39fce362e300ead671f5feb07e82b202d994b517
Marcelo Cabral <cabralme@gmail.com>
2024-05-19 16:03:40 -0300

    Profile, H2 e JPA

--- src/main/java/com/cabralme/cadastropix/entities/Conta.java
+++ src/main/java/com/cabralme/cadastropix/entities/Conta.java
@@ -4,9 +4,19 @@ import java.io.Serializable;
 import java.util.Objects;
 import java.util.UUID;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_conta")
 public class Conta implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
 	private UUID id;
 	private Integer numeroconta;
 	private Integer numeroagencia;
